3b9a7c300318a39b4572dc04795699abad4a1129
Evghenii Gaburov <egaburov@dds.nl>
2016-01-10 19:46:28 -0800

    replaced with other iterative radiance

--- agency/smallpt.cpp
+++ agency/smallpt.cpp
@@ -61,7 +61,7 @@ struct Ray
   Ray(Vec o_, Vec d_) : o(o_), d(d_) {} 
 };
 
-enum Refl_t 
+enum class Refl_t 
 { 
   DIFF, 
   SPEC, 
@@ -92,15 +92,15 @@ struct Sphere
 };
 Sphere spheres[] = 
 {//Scene: radius, position, emission, color, material
-  Sphere((RR), Vec( (RR)+1,40.8,81.6), Vec(),Vec(.75,.25,.25),DIFF),//Left
-  Sphere((RR), Vec(-(RR)+99,40.8,81.6),Vec(),Vec(.25,.25,.75),DIFF),//Rght
-  Sphere((RR), Vec(50,40.8, (RR)),     Vec(),Vec(.75,.75,.75),DIFF),//Back
-  Sphere((RR), Vec(50,40.8,-(RR)+170), Vec(),Vec(),           DIFF),//Frnt
-  Sphere((RR), Vec(50, (RR), 81.6),    Vec(),Vec(.75,.75,.75),DIFF),//Botm
-  Sphere((RR), Vec(50,-(RR)+81.6,81.6),Vec(),Vec(.75,.75,.75),DIFF),//Top
-  Sphere(16.5,Vec(27,16.5,47),       Vec(),Vec(1,1,1)*.999, SPEC),//Mirr
-  Sphere(16.5,Vec(73,16.5,78),       Vec(),Vec(1,1,1)*.999, REFR),//Glas
-  Sphere(600, Vec(50,681.6-.27,81.6),Vec(12,12,12),  Vec(), DIFF) //Lite
+  Sphere((RR), Vec( (RR)+1,40.8,81.6), Vec(),Vec(.75,.25,.25),Refl_t::DIFF),//Left
+  Sphere((RR), Vec(-(RR)+99,40.8,81.6),Vec(),Vec(.25,.25,.75),Refl_t::DIFF),//Rght
+  Sphere((RR), Vec(50,40.8, (RR)),     Vec(),Vec(.75,.75,.75),Refl_t::DIFF),//Back
+  Sphere((RR), Vec(50,40.8,-(RR)+170), Vec(),Vec(),           Refl_t::DIFF),//Frnt
+  Sphere((RR), Vec(50, (RR), 81.6),    Vec(),Vec(.75,.75,.75),Refl_t::DIFF),//Botm
+  Sphere((RR), Vec(50,-(RR)+81.6,81.6),Vec(),Vec(.75,.75,.75),Refl_t::DIFF),//Top
+  Sphere(16.5,Vec(27,16.5,47),       Vec(),Vec(1,1,1)*.999, Refl_t::SPEC),//Mirr
+  Sphere(16.5,Vec(73,16.5,78),       Vec(),Vec(1,1,1)*.999, Refl_t::REFR),//Glas
+  Sphere(600, Vec(50,681.6-.27,81.6),Vec(12,12,12),  Vec(), Refl_t::DIFF) //Lite
 };
 
 using vec_t = Vec;
@@ -136,119 +136,64 @@ static inline bool intersect(const Ray &r, real &t, int &id)
   return t<inf;
 }
 
-static Vec radiance(Ray r, int depth, unsigned short *Xi, Rand48 &rr)
+Vec radiance(const Ray &r_, int depth_, Rand48 &rr)
 {
-  using pair1 = std::pair<Ray,int>;
-  using pair2 = std::pair<Vec,Vec>;
-
-  std::vector<pair2> partial;
-
-  partial.reserve(128);
-
-  constexpr int ndepth = 32;
-  for (int depth = 0; depth < ndepth; depth++)
-  {
-    real t;                               // distance to intersection
-    int id=0;                               // id of intersected object
-    if (!intersect(r, t, id)) 
-    {
-      break;
-    }
-
+  real t;                               // distance to intersection
+  int id=0;                               // id of intersected object
+  Ray r=r_;
+  int depth=depth_;
+  // L0 = Le0 + f0*(L1)
+  //    = Le0 + f0*(Le1 + f1*L2)
+  //    = Le0 + f0*(Le1 + f1*(Le2 + f2*(L3))
+  //    = Le0 + f0*(Le1 + f1*(Le2 + f2*(Le3 + f3*(L4)))
+  //    = ...
+  //    = Le0 + f0*Le1 + f0*f1*Le2 + f0*f1*f2*Le3 + f0*f1*f2*f3*Le4 + ...
+  // 
+  // So:
+  // F = 1
+  // while (1){
+  //   L += F*Lei
+  //   F *= fi
+  // }
+  Vec cl(0,0,0);   // accumulated color
+  Vec cf(1,1,1);  // accumulated reflectance
+  while (1){
+    if (!intersect(r, t, id)) return cl; // if miss, return black
     const Sphere &obj = spheres[id];        // the hit object
-
-    Vec x = r.o+r.d*t;
-    Vec n = (x-obj.p).norm();
-    Vec nl = n.dot(r.d) <0 ? n : n*(-1);
-    Vec f=obj.c;
-    real p = (f.x>f.y && f.x>f.z) ? f.x : (f.y>f.z ? f.y : f.z); // max refl
-
-    if (depth>=5) 
-    {
-      if (rr.drand()<p) 
-      {
-        f=f*(1/p); 
-      }
-      else 
-      {
-        partial.emplace_back(obj.e, Vec());
-        break;
-      }
-    }
-
-    if (obj.refl == SPEC)            // Ideal SPECULAR reflection
-    {
-      partial.emplace_back(obj.e, f);
-      r = Ray(x,r.d-n*2*n.dot(r.d));
-    }
-    else if (obj.refl == DIFF)
-    {                  // Ideal DIFFUSE reflection
-      real r1=2*M_PI*rr.drand();
-      real r2=rr.drand();
-      real r2s=sqrt(r2);
-
-      Vec w=nl;
-      Vec u=( (fabs(w.x)>.1 ? Vec(0,1) : Vec(1))%w ).norm(); 
-      Vec v=w%u;
+    Vec x=r.o+r.d*t, n=(x-obj.p).norm(), nl=n.dot(r.d)<0?n:n*-1, f=obj.c;
+    real p = f.x>f.y && f.x>f.z ? f.x : f.y>f.z ? f.y : f.z; // max refl
+    cl = cl + cf.mult(obj.e);
+    if (++depth>5) if (rr.drand()<p) f=f*(1/p); else return cl; //R.R.
+    cf = cf.mult(f);
+    if (obj.refl == Refl_t::DIFF){                  // Ideal DIFFUSE reflection
+      real r1=2*M_PI*rr.drand(), r2=rr.drand(), r2s=sqrt(r2);
+      Vec w=nl, u=((fabs(w.x)>.1?Vec(0,1):Vec(1))%w).norm(), v=w%u;
       Vec d = (u*cos(r1)*r2s + v*sin(r1)*r2s + w*sqrt(1-r2)).norm();
-
-      partial.emplace_back(obj.e, f);
       r = Ray(x,d);
-    } 
-    else
-    {
-      Ray reflRay(x, r.d-n*2*n.dot(r.d));     // Ideal dielectric REFRACTION
-      bool into = n.dot(nl)>0;                // Ray from outside going in?
-      real nc=1;
-      real nt=1.5;
-      real nnt=into?nc/nt:nt/nc;
-      real ddn=r.d.dot(nl);
-      real cos2t;
-
-      if ((cos2t=1-nnt*nnt*(1-ddn*ddn))<0)    // Total internal reflection
-      {
-        partial.emplace_back(obj.e,f);
-        r = reflRay;
-      }
-      else
-      {
-        Vec tdir = (r.d*nnt - n*( (into?1:-1)*(ddn*nnt+sqrt(cos2t))) ).norm();
-
-        real a=nt-nc;
-        real b=nt+nc;
-        real R0=a*a/(b*b);
-        real c = 1-(into?-ddn:tdir.dot(n));
-
-        real Re=R0+(1-R0)*c*c*c*c*c;
-        real Tr=1-Re;
-        real P = .25 + .5 * Re;
-        real RP=Re/P;
-        real TP=Tr/(1-P);
-
-        if (rr.drand() < P)
-        {
-          partial.emplace_back(obj.e, Vec(RP,RP,RP));
-          r = reflRay;
-        }
-        else
-        {
-          partial.emplace_back(obj.e, Vec(TP,TP,TP));
-          r = Ray(x,tdir);
-        }
-      }
+      continue;
+    } else if (obj.refl == Refl_t::SPEC){           // Ideal SPECULAR reflection
+      r = Ray(x,r.d-n*2*n.dot(r.d));
+      continue;
     }
+    Ray reflRay(x, r.d-n*2*n.dot(r.d));     // Ideal dielectric REFRACTION
+    bool into = n.dot(nl)>0;                // Ray from outside going in?
+    real nc=1, nt=1.5, nnt=into?nc/nt:nt/nc, ddn=r.d.dot(nl), cos2t;
+    if ((cos2t=1-nnt*nnt*(1-ddn*ddn))<0){    // Total internal reflection
+      r = reflRay;
+      continue;
+    }
+    Vec tdir = (r.d*nnt - n*((into?1:-1)*(ddn*nnt+sqrt(cos2t)))).norm();
+    real a=nt-nc, b=nt+nc, R0=a*a/(b*b), c = 1-(into?-ddn:tdir.dot(n));
+    real Re=R0+(1-R0)*c*c*c*c*c,Tr=1-Re,P=.25+.5*Re,RP=Re/P,TP=Tr/(1-P);
+    if (rr.drand()<P){
+      cf = cf*RP;
+      r = reflRay;
+    } else {
+      cf = cf*TP;
+      r = Ray(x,tdir);
+    }
+    continue;
   }
-
-  Vec col = Vec();
-  const int n = partial.size();
-  for (int i = n-1; i >= 0; i--)
-  {
-    auto p = partial[i];
-    col = p.first + p.second.mult(col);
-  }
-  return col;
-
-
 }
 
 static inline real clamp(real x)
@@ -274,32 +219,31 @@ int main(int argc, char *argv[])
   Vec r;
   Vec *c=new Vec[w*h];
 
-
-  unsigned short Xi[3];
+  for (int s = 0; s < samps; s++)
   {
-    for (int s=0; s<samps; s++)
-    {
-      fprintf(stderr,"\rRendering (%d spp) : %d ",samps*4, s*4);
+    fprintf(stderr, "\rRendering (%d spp) : %d ", samps * 4, s * 4);
 
 #pragma omp parallel for schedule(guided)
-      for (int y=0; y<h; y++)
-        for (unsigned short x=0; x<w; x++)   // Loop cols
-        {
-          static Rand48 rr(omp_get_thread_num());
-          Vec r = Vec();
-          const int idx = (h-y-1)*w+x;
-          for (int sy=0; sy<2; sy++)     // 2x2 subpixel rows
-            for (int sx=0; sx<2; sx++)
-            {        // 2x2 subpixel cols
-              real r1=2*rr.drand(), dx=r1<1 ? sqrt(r1)-1: 1-sqrt(2-r1);
-              real r2=2*rr.drand(), dy=r2<1 ? sqrt(r2)-1: 1-sqrt(2-r2);
-              Vec d = cx*( ( (sx+.5 + dx)/2 + x)/w - .5) +
-                cy*( ( (sy+.5 + dy)/2 + y)/h - .5) + cam.d;
-              r = r + radiance(Ray(cam.o+d*140,d.norm()),0,Xi,rr); //*(1./samps);
-            } 
-          c[idx] = c[idx] + r;
-        }
-    }
+    for (int y = 0; y < h; y++)
+      for (unsigned short x = 0; x < w; x++) // Loop cols
+      {
+        static Rand48 rr(omp_get_thread_num());
+        Vec r = Vec();
+        const int idx = (h - y - 1) * w + x;
+        for (int sy = 0; sy < 2; sy++) // 2x2 subpixel rows
+          for (int sx = 0; sx < 2; sx++)
+          { // 2x2 subpixel cols
+            real r1 = 2 * rr.drand(),
+                 dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
+            real r2 = 2 * rr.drand(),
+                 dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
+            Vec d = cx * (((sx + .5 + dx) / 2 + x) / w - .5) +
+                    cy * (((sy + .5 + dy) / 2 + y) / h - .5) + cam.d;
+            r = r +
+                radiance(Ray(cam.o + d * 140, d.norm()), 0, rr); //*(1./samps);
+          }
+        c[idx] = c[idx] + r;
+      }
   }
 
   for (int i = 0; i < w*h; i++)
