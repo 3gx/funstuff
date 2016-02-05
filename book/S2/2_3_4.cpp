34306adb581a964b0c27dd9406ebf8aadd7366eb
Evghenii Gaburov <egaburov@dds.nl>
2016-02-05 12:09:37 -0800

    reduce code duplication

--- book/S2/2_3_4.cpp
+++ book/S2/2_3_4.cpp
@@ -24,63 +24,70 @@ struct type_to_string<long int>
   static constexpr auto  value = "long int";
 };
 
-template<class T>
+
+struct Const 
+{
+  static constexpr auto value = " const";
+};
+struct Volatile 
+{
+  static constexpr auto value = " volatile";
+};
+struct Pointer
+{
+  static constexpr auto value = "*";
+};
+struct LValRef
+{
+  static constexpr auto value = "&";
+};
+struct RValRef
+{
+  static constexpr auto value = "&&";
+};
+
+
+template<class...>
+struct QualPrinter
+{
+  friend void operator<<(std::ostream &os, const QualPrinter &) {}
+};
+
+template<class Qual, class... Quals>
+struct QualPrinter<Qual, Quals...>
+{
+  friend std::ostream& operator<<(std::ostream& os, const QualPrinter&)
+  {
+    os << Qual::value << QualPrinter<Quals...>();
+    return os;
+  }
+};
+
+
+template<class Type, class... Quals>
 struct type_descriptor
 {
   friend std::ostream& operator<<(std::ostream& os, const type_descriptor&)
   {
-    os << type_to_string<T>::value;
+    os << type_to_string<Type>::value << QualPrinter<Quals...>();
     return os;
   }
 };
 
-template<class T>
-struct type_descriptor<T const>
-{
-  friend std::ostream& operator<<(std::ostream& os, const type_descriptor&)
-  {
-    os << type_descriptor<T>() << " const";
-    return os;
-  }
-};
+template <class T, class... Quals>
+struct type_descriptor<T const, Quals...> : type_descriptor<T, Const, Quals...> {};
 
-template<class T>
-struct type_descriptor<T *>
-{
-  friend std::ostream& operator<<(std::ostream& os, const type_descriptor&)
-  {
-    os << type_descriptor<T>() << "*";
-    return os;
-  }
-};
-template<class T>
-struct type_descriptor<T &>
-{
-  friend std::ostream& operator<<(std::ostream& os, const type_descriptor&)
-  {
-    os << type_descriptor<T>() << "&";
-    return os;
-  }
-};
-template<class T>
-struct type_descriptor<T &&>
-{
-  friend std::ostream& operator<<(std::ostream& os, const type_descriptor&)
-  {
-    os << type_descriptor<T>() << "&&";
-    return os;
-  }
-};
+template <class T, class... Quals>
+struct type_descriptor<T volatile, Quals...> : type_descriptor<T, Volatile, Quals...> {};
 
-template<class T>
-struct type_descriptor<T volatile>
-{
-  friend std::ostream& operator<<(std::ostream& os, const type_descriptor&)
-  {
-    os << type_descriptor<T>() << " volatile";
-    return os;
-  }
-};
+template <class T, class... Quals>
+struct type_descriptor<T *, Quals...> : type_descriptor<T, Pointer, Quals...> {};
+
+template <class T, class... Quals>
+struct type_descriptor<T &, Quals...> : type_descriptor<T, LValRef, Quals...> {};
+
+template <class T, class... Quals>
+struct type_descriptor<T &&, Quals...> : type_descriptor<T, RValRef, Quals...> {};
 
 
 int main()
@@ -95,5 +102,5 @@ int main()
   cout << type_descriptor<int const*&& >() << endl;
   cout << type_descriptor<int const* volatile >() << endl;
   cout << type_descriptor<int * volatile >() << endl;
-//  cout << type_descriptor<long const* volatile>() << endl;
+  cout << type_descriptor<long const* volatile>() << endl;
 }
