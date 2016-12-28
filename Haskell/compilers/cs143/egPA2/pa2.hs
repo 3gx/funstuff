a942c917653732efd1be3330826e566c7ed5e7e5
Evghenii Gaburov <egaburov@dds.nl>
2016-12-28 11:37:36 -0800

    added Makefile

--- Haskell/compilers/cs143/egPA2/pa2.hs
+++ Haskell/compilers/cs143/egPA2/pa2.hs
@@ -1,1 +1,14 @@
 -- Programming Assignment I --
+
+import System.Environment
+
+printString :: String -> IO()
+printString s = do
+  print s
+
+
+main :: IO ()
+main = do
+  [f] <- getArgs
+  source <- readFile f
+  print source
