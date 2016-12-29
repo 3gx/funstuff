2267241372775ac8b740116e493fccde9aa6c79b
Evghenii Gaburov <egaburov@dds.nl>
2016-12-29 10:06:17 -0800

    +1

--- Haskell/BartoszBofH/5_Tokenizer/tokenize.hs
+++ Haskell/BartoszBofH/5_Tokenizer/tokenize.hs
@@ -30,6 +30,44 @@ showContent (TokNum i) = show i
 token :: Token
 token = TokIdent "x"
 
+operator :: Char -> Operator
+operator c | c == '+' = Plus
+           | c == '-' = Minus
+           | c == '*' = Times
+           | c == '/' = Div
+
+
+
+tokenize :: String -> [Token]
+tokenize [] = []
+tokenize (c:cs) 
+  | elem c "+/-*" = TokOp (operator c) : tokenize cs
+  | isDigit c     = TokNum (digitToInt c) : tokenize cs
+  | isAlpha c     = TokIdent [c] : tokenize cs
+  | isSpace c     = tokenize cs
+  | otherwise = error $ "Cannot tokenize " ++ [c]
+
+isDigit :: Char -> Bool
+isDigit c = elem c ['0'..'9']
+
+isAlpha :: Char -> Bool
+isAlpha c = elem c $ ['a'..'z'] ++ ['A'..'Z']
+
+isSpace :: Char -> Bool
+isSpace c = elem c $ " "
+
+digitToInt :: Char -> Int
+digitToInt c | isDigit c = fromEnum c - 48
+
+digitToInts :: String -> [Int]
+digitToInts [] = []
+digitToInts (x:xs) = digitToInt x : digitToInts xs
+
+
 main = do 
   putStrLn $ showContent token
   print token
+  print $ operator '*'
+  print $ tokenize "**/+"
+  print $ tokenize "1 + 4 / x"
+  print $ digitToInts "1234"
