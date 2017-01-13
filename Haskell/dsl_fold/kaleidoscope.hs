1561c4d0f3ef9d9289257a28e97cdf8b4d2d8426
Evghenii Gaburov <egaburov@dds.nl>
2017-01-13 08:11:33 -0800

    first commit for kaleidsocope

---
+++ Haskell/dsl_fold/kaleidoscope.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE DeriveFunctor, UndecidableInstances #-}
+
+{- simple program
+program = do
+  extern sin(arg);
+  
+  sin(1.5705);
+
+-- should return  0.9999999560952156, close to 1
+-}
+
+
