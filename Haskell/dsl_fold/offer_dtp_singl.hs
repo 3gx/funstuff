57986b6f4c3d23a56fff7bb1f090ce81a80f2867
Evghenii Gaburov <egaburov@dds.nl>
2017-01-15 15:00:57 -0800

    added Vect

--- Haskell/dsl_fold/offer_dtp_singl.hs
+++ Haskell/dsl_fold/offer_dtp_singl.hs
@@ -1,1 +1,10 @@
-data Nat = Zero | Succ Nat
+{-# LANGUAGE GADTs
+            ,DataKinds 
+#-}
+
+data Nat = Zero | Succ Nat 
+
+data Vect n a where
+  -- requires DataKinds
+  VNil :: Vect Zero a
+  VCons :: a -> Vect n a -> Vect (Succ n) a
