26a1e194d51a902b1e11e8f623dcab0c04689cb9
Evghenii Gaburov <egaburov@dds.nl>
2017-01-15 12:39:59 -0800

    working on TF

--- Haskell/dsl_fold/offer_tf.hs
+++ Haskell/dsl_fold/offer_tf.hs
@@ -3,17 +3,6 @@
              TypeFamilies
 #-}
 
-data Offer a = Present a
-             | PercentDiscount Float
-             | AbsoluteDiscount Float 
-             | Restrict [a] (Offer a)
-             | From Integer (Offer a) 
-             | Until Integer (Offer a)
-             | Extend Integer (Offer a)
-             | Both (Offer a) (Offer a)
-             | BetterOf (Offer a) (Offer a)
---             | If (Expr a) (Offer a) (Offer a)
-           deriving (Show)
 
 data Expr a r where
   AmountOf            :: a -> Expr a Integer
@@ -32,26 +21,6 @@ data Expr a r where
   Not                 :: Expr a Bool -> Expr a Bool 
  
   
-noOffer :: Offer a
-noOffer = AbsoluteDiscount 0
-
-{-
-v :: Offer String
-v = Until 30 $ BetterOf (AbsoluteDiscount 10.0)
-                          (Both (Present "ballon")
-                          (If (TotalPrice :>: IVal 100) (PercentDiscount 5.0)
-                                                         noOffer)) 
--}
-
-period :: Integer -> Integer -> Offer a -> Offer a
-period f d o = From d (Until (f+d) o)
-
-
-allOf :: [a] -> Offer a
-allOf os = Restrict os  noOffer
-
-v1 = period 3 5 (Both (Both (Present "ballon") (Present "choco muffin")) (PercentDiscount 10.0))
-
 -- oops, illegal expression, but it still type checks
 {- doesn't type check with GADTs
 incorrectExpression :: Expr Char
@@ -90,4 +59,9 @@ data Vect n a where
 type family Plus x y where
   Plus Zero x = x
   Plus (Succ x) y = Succ (Plus x y)
+
+data Offer a p where
+  Present          :: a -> Offer a (Succ Zero)
+  PercentDiscount  :: Float -> Offer a Zero
+  AbsoluteDiscount :: Float -> Offer a Zero
 
