bef5885a07d25207bdfeb8904149e33f0bcfc41c
Evghenii Gaburov <egaburov@dds.nl>
2017-01-15 16:20:06 -0800

    some more stuff

--- Haskell/dsl_fold/offer_dtp_singl_th.hs
+++ Haskell/dsl_fold/offer_dtp_singl_th.hs
@@ -5,9 +5,13 @@
 --import Data.Singletons -- .TH
 import Data.Singletons.TH hiding (Min, Compare, CompareSym0)
 
-$(promote [d|
+$(singletons [d|
   data Nat = Zero | Succ Nat
            deriving (Show, Eq)
+  |])
+
+$(promote [d|
+
   plus :: Nat -> Nat -> Nat
   plus Zero     y = y
   plus (Succ x) y = Succ (plus x y)
@@ -52,12 +56,47 @@ $(promote [d|
   restrictFrom2 n _ t Greater Less    = Closed n t
   restrictFrom2 _ f t Equal   _       = Closed f t
   restrictFrom2 _ f t Less    _       = Closed f t
+
+  restrictUntil :: Nat -> Range -> Range
+  restrictUntil _ Empty    = Empty
+  restrictUntil n (Open f) = restrictUntil1 n f (compare n f)
+  restrictUntil n (Closed f t) = restrictUntil2 n f t (compare n f) (compare n t)
+
+  restrictUntil1 :: Nat -> Nat -> Comparison -> Range
+  restrictUntil1 n f Greater = Closed f n
+  restrictUntil1 _ _ Equal   = Empty
+  restrictUntil1 _ _ Less    = Empty
+
+  restrictUntil2 :: Nat -> Nat -> Nat -> Comparison -> Comparison -> Range
+  restrictUntil2 _ f t _       Greater = Closed f t
+  restrictUntil2 _ f t _       Equal   = Closed f t
+  restrictUntil2 n f _ Greater Less    = Closed f n
+  restrictUntil2 _ _ _ Equal   Less    = Empty
+  restrictUntil2 _ _ _ Less    Less    = Empty
   |])
 
-{-
-$(singletons [d|
-  data Nat = Zero | Succ Nat
-           deriving (Show, Eq)
-  |])
--}
+data Offer a (r :: Range) where
+  Present          :: a -> Offer a Infinite
+  PercentDiscount  :: Float -> Offer a Infinite
+  AbsoluteDisocunt :: Float -> Offer a Infinite
+  From             :: SNat n -> Offer a d -> Offer a (RestrictFrom n d)
+  Until            :: SNat n -> Offer a d -> Offer a (RestrictUntil n d)
+
+toNat :: SNat n -> Nat
+toNat SZero = Zero
+toNat (SSucc n) = Succ (toNat n)
+
+printDateRestriction :: Offer a r -> String
+printDateRestriction (From n _)  = "From " ++ show (toNat n)
+printDateRestriction (Until n _) = "Until" ++ show (toNat n)
+printDateRestriction _           = "No date restriction"
+
+zero :: SNat Zero
+zero = sing   -- results in SZero
+one :: SNat (Succ Zero)
+one = sing    -- results in SSucc SZero
+two :: SNat (Succ (Succ Zero))
+two = sing    -- results in SSucc (SSucc SZero)
+three :: SNat (Succ (Succ (Succ Zero)))
+three = sing  -- results in SSucc (SSucc (SSucc SZero))
 
