d5db20c3960ea1c173f5175e8dbd09b38885fb5c
Evghenii Gaburov <egaburov@dds.nl>
2017-01-15 16:05:12 -0800

    +1

--- Haskell/dsl_fold/offer_dtp_singl_th.hs
+++ Haskell/dsl_fold/offer_dtp_singl_th.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE UndecidableInstances #-}
 
 --import Data.Singletons -- .TH
-import Data.Singletons.TH hiding (Min)
+import Data.Singletons.TH hiding (Min, Compare, CompareSym0)
 
 $(promote [d|
   data Nat = Zero | Succ Nat
@@ -24,6 +24,35 @@ $(promote [d|
   infinite = Open Zero
   |])
 
+
+$(promote [d|
+  data Comparison = Less | Equal | Greater
+
+  -- conflicts with Data.Singleton.TH.Compare & CompareSym0
+  -- hiding when importing Data.Singleton.TH module
+  compare :: Nat -> Nat -> Comparison
+  compare Zero     Zero     = Equal
+  compare Zero     (Succ _) = Less
+  compare (Succ _) Zero     = Greater
+  compare (Succ x) (Succ y) = compare x y
+
+  restrictFrom :: Nat -> Range -> Range
+  restrictFrom _ Empty    = Empty
+  restrictFrom n (Open f) = restrictFrom1 n f (compare n f)
+  restrictFrom n (Closed f t) = restrictFrom2 n f t (compare n f) (compare n t)
+
+  restrictFrom1 :: Nat -> Nat -> Comparison -> Range
+  restrictFrom1 n _ Greater = Open n
+  restrictFrom1 _ f Equal   = Open f
+  restrictFrom1 _ f Less    = Open f
+
+  restrictFrom2 :: Nat -> Nat -> Nat -> Comparison -> Comparison -> Range
+  restrictFrom2 _ _ _ Greater Greater = Empty
+  restrictFrom2 _ _ _ Greater Equal   = Empty
+  restrictFrom2 n _ t Greater Less    = Closed n t
+  restrictFrom2 _ f t Equal   _       = Closed f t
+  restrictFrom2 _ f t Less    _       = Closed f t
+  |])
 
 {-
 $(singletons [d|
