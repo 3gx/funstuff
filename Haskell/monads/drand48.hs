3a78c03850add2d4d4fb507e4b762e1d096ecc1a
Evghenii <eg0x52@gmail.com>
2017-02-07 19:29:56 -0800

    monads

---
+++ Haskell/monads/drand48.hs
@@ -0,0 +1,45 @@
+newtype State s a = State { runState :: s -> (a,s) };
+
+returnState :: a -> State s a
+returnState a = State $ \s -> (a,s)
+
+bindState :: State s a -> (a -> State s b) -> State s b
+bindState m k = State $ \s -> let (a, s') = runState m s
+                              in runState (k a) s'
+
+updateState :: Int -> Int
+updateState stat = stat * 0x5DEECE66D + 0xB;
+
+{-
+ 
+  struct Rand48
+  {
+    double drand()
+    {
+      update();
+      return (stat&0xFFFFFFFFFFFF)*(1.0/281474976710656.0);
+    }
+    long lrand()
+    {
+      update();
+      return (long)(stat>>17)&0x7FFFFFFF;
+    }
+    long mrand()
+    {
+      update();
+      return(long)(stat>>16)&0xFFFFFFFF;
+    }
+    void srand(const long seed)
+    {
+      stat = (seed<<16)+0x330E;
+    }
+
+    private:
+    long long stat;
+    void update()
+    {
+      stat = stat*0x5DEECE66D + 0xB;
+    }
+  };
+
+-}
