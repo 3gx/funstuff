e1313514b87d46e1c852c470537862689cf4fb02
Evghenii <eg0x52@gmail.com>
2017-02-08 12:16:13 -0800

    going to work on mutable data

--- Haskell/monads/io.hs
+++ Haskell/monads/io.hs
@@ -1,4 +1,4 @@
-import Data.Char (chr)
+import Data.Char (chr,toUpper)
 
 -- simple sequences execution examples
 
@@ -18,4 +18,47 @@ get4chars i0 = (a++b, i2) where (a,i1) = get2chars i0
 
 -----
 
+mainx world0 = 
+            let (a,world1) = getchar world0
+                (b,world2) = getchar world1
+            in ((), world2)
 
+ask :: String -> IO Int
+ask s = putStr s >> readLn;
+
+ 
+
+
+when :: Bool -> IO () -> IO () -> IO ()
+when condition action world = 
+  if condition 
+    then action
+    else world
+
+(action1 .|>. action2) world0 =
+   let (a, world1) = action1 world0
+       (b, world2) = action2 world1
+   in (b, world2)
+
+
+action1 .>>. action2 = action
+  where
+    action world0 = let (a, world1) = action1 world0
+                        (b, world2) = action2 world1
+                    in (b, world2)
+
+liftm :: (a->b) -> IO a -> IO b
+liftm f action = action >>= \x -> return (f x)
+
+
+touppers :: String -> String
+touppers (x:xs) | null xs = [toUpper x] | otherwise = toUpper x :touppers xs
+
+touppersm = liftm touppers
+
+main = do  
+      str <- (touppersm.return) "wow!"
+      print str
+      a <- ask "Whats1?"
+      b <- ask "Whats2?"
+      return ();
