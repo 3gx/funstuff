007e55da1c24477313b79da0ec25fcef8c31606e
Evghenii <eg0x52@gmail.com>
2017-02-09 08:31:48 -0800

    added example

--- Haskell/monads/writer.hs
+++ Haskell/monads/writer.hs
@@ -58,6 +58,19 @@ finalCountDown' x = do
     finalCountDown' (x-1)  
     tell [show x]  
 
+keepSmall :: Int -> Writer [String] Bool  
+keepSmall x  
+    | x < 4 = do  
+        tell ["Keeping " ++ show x]  
+        return True  
+    | otherwise = do  
+        tell [show x ++ " is too large, throwing it away"]  
+        return False 
+
 main = do
   mapM_ putStrLn . fromDiffList . snd . runWriter $ gcd'' 110 34
+  print $ fst $ runWriter $ filterM  keepSmall [9,1,5,2,1,3]
+  mapM_ putStrLn .  snd $ runWriter $ filterM  keepSmall [9,1,5,2,1,3]
+
+
 
