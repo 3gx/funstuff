e01ddc6cac76269f9c3071a93048ecda252f5a63
Evghenii <eg0x52@gmail.com>
2017-02-10 08:47:37 -0800

    added tokenizer

--- Haskell/book/parser.hs
+++ Haskell/book/parser.hs
@@ -95,6 +95,124 @@ instance Alternative Parser where
 
 -- 13.6 Derived primitives
 
+sat :: (Char -> Bool) -> Parser Char
+sat p = do 
+      x <- item
+      if (p x) then return x else empty
+
+digit :: Parser Char
+digit = sat isDigit
+
+lower :: Parser Char
+lower = sat isLower
+
+upper :: Parser Char
+upper = sat isUpper
+
+letter :: Parser Char
+letter = sat isAlpha
+
+alphanum :: Parser Char
+alphanum = sat isAlphaNum
+
+char :: Char -> Parser Char
+char x = sat (== x)
+
+-- > parse (char 'a') "abc"
+-- [('a',"bc")]
+-- > parse (char 'a') "xabc"
+-- []
+
+string :: String -> Parser String
+string [] = return []
+string (x:xs) = do 
+    char x
+    string xs
+    return (x:xs)
+
+--  (string "xab") "xabc"
+--  [("xab","c")]
+--  > parse (string "xaa") "xabc"
+--  []
+
+-- > parse (many digit) "1123abc"
+-- [("1123","abc")]
+-- > parse (many digit) "a1123abc"
+-- [("","a1123abc")]
+-- > parse (some digit) "a1123abc"
+-- []
+
+ident :: Parser String
+ident = do 
+    x <- lower
+    xs <- many alphanum
+    return (x:xs)
+
+nat :: Parser Int
+nat = do
+  xs <- some digit
+  return (read xs)
+
+space :: Parser ()
+space = do
+  many (sat isSpace)
+  return ()
+
+int :: Parser Int
+int = do char '-'
+         n <- nat
+         return (-n)
+      <|> nat
+
+-- > parse ident "abc def"
+-- [("abc"," def")]
+-- > parse ident "Abc def"
+-- []
+-- > parse space " abc"
+-- [((),"abc")]
+-- > parse space "  abc"
+-- [((),"abc")]
+-- > parse nat "123 abc"
+-- [(123," abc")]
+-- > parse nat "-123 abc"
+-- []
+-- > parse int "-123 abc"
+-- [(-123," abc")]
+-- > parse int "123 abc"
+-- [(123," abc")]
 
 
+-- 13.7 Handling spaces
 
+token :: Parser a -> Parser a 
+token p = do space
+             v <- p
+             space
+             return v 
+
+identifier :: Parser String
+identifier = token ident
+
+natural :: Parser Int
+natural = token nat
+
+integer :: Parser Int
+integer = token int
+
+symbol :: String -> Parser String
+symbol xs = token (string xs)
+
+nats :: Parser [Int]
+nats = do symbol "["
+          n <- natural
+          ns <- many (do {symbol ","; natural})
+          symbol "]"
+          return (n:ns)
+
+--  > parse nats "[1, 2,  3, 4]"
+--  [([1,2,3,4],"")]
+--  > parse nats "[1, 2,  3, 4,]"
+--  []
+
+
+-- 13.8 Arithmetic expressions
