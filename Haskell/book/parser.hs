209cb8ba81af998f27c8479b310983af455e74a2
Evghenii <eg0x52@gmail.com>
2017-02-10 09:13:20 -0800

    added simple parser + evaluator

--- Haskell/book/parser.hs
+++ Haskell/book/parser.hs
@@ -216,3 +216,41 @@ nats = do symbol "["
 
 
 -- 13.8 Arithmetic expressions
+
+{-
+ Grammar
+---------
+   expr ::= term ( + expr | eps)
+   term ::= factor (*term | esp)
+   factor ::= ( expr ) | nat
+   nat ::= 0 |1 |2 |...
+-}
+
+expr :: Parser Int
+expr = do t <- term
+          do symbol "+"
+             e <- expr
+             return (t + e)
+             <|> return t
+
+term :: Parser Int
+term = do f <- factor
+          do symbol "*"
+             t <- term
+             return (f*t)
+             <|> return f
+
+factor :: Parser Int
+factor = do symbol "("
+            e <- expr
+            symbol ")"
+            return e
+         <|> natural
+
+
+eval :: String -> Int
+eval xs = case (parse expr xs) of
+              [(n,[])] -> n
+              [(_,out)] -> error ("Unused input " ++ out)
+              [] -> error "Invalid input"
+
