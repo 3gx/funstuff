175e805d43712b1835eae52af612400940af3505
Evghenii <eg0x52@gmail.com>
2017-02-10 07:03:09 -0800

    added perse item

--- Haskell/book/parser.hs
+++ Haskell/book/parser.hs
@@ -1,1 +1,25 @@
--- Monadic parsing
+-- Ch 13: Monadic parsing
+
+
+-- type Parser = String -> (Tree,String)
+--                |__________|____|_________ input
+--                           |____|_________ parse tree
+--                                |_________ rest of the string
+import Control.Applicative
+import Data.Char
+
+newtype Parser a = P (String -> [(a,String)])
+
+parse :: Parser a -> String -> [(a,String)]
+parse (P p) inp = p inp
+
+item :: Parser Char
+item = P (\inp -> case inp of []     -> []
+                              (x:xs) -> [(x,xs)])
+
+-- > parse item ""
+-- []
+-- > parse item "abc"
+-- [('a',"bc")]
+--
+
