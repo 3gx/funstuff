2277a4f78de6783c996d8c572ba3373eeb795a49
Evghenii <eg0x52@gmail.com>
2017-02-10 13:20:30 -0800

    +1

--- Haskell/book/14_foldables.hs
+++ Haskell/book/14_foldables.hs
@@ -1,3 +1,46 @@
 -- 14.1 Monoids
 
+{-
+  class Monoid a where
+    mempty :: a
+    mappend :: a -> a -> a
+
+    mconcat :: [a] -> a
+    mconcat = foldr mappend mempry
+-}
+
+
+instance Monoid Int where
+  mempty = 0
+  mappend = (+)
+
+-- > (3::Int) `mappend` 5
+-- 8
+-- > (3::Int) <> 5
+-- 8
+
+
+instance Monoid Integer where
+  mempty = 1
+  mappend = (*)
+
+-- > 3 `mappend` 5
+-- 15
+-- > 3 <> 5
+-- 15
+
+
+
+-- fold behaves in the same way mconcat 
+
+fold :: Monoid a => [a] -> a
+fold [] = mempty
+fold (x:xs) = x `mappend` fold xs
+
+-- > fold [3,4,5,6,7]
+-- 2520
+-- > fold [3::Int,4,5,6,7]
+-- 25
+
+
 
