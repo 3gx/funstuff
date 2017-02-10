d92f31e23747165dccebd76972f42cee682057b4
Evghenii <eg0x52@gmail.com>
2017-02-10 13:01:17 -0800

    cmpleted calc

--- Haskell/book/parser.hs
+++ Haskell/book/parser.hs
@@ -7,6 +7,7 @@
 --                                |_________ rest of the string
 import Control.Applicative
 import Data.Char
+import System.IO
 
 newtype Parser a = P { parse :: String -> [(a,String)] }
 
@@ -221,7 +222,7 @@ nats = do symbol "["
  Grammar
 ---------
    expr ::= term ( + expr | eps)
-   term ::= factor (*term | esp)
+   term ::= factor (*term | eps)
    factor ::= ( expr ) | nat
    nat ::= 0 |1 |2 |...
 -}
@@ -254,3 +255,93 @@ eval xs = case (parse expr xs) of
               [(_,out)] -> error ("Unused input " ++ out)
               [] -> error "Invalid input"
 
+-- 13.9 Calculator
+
+box :: [String]
+box = ["+---------------+",
+       "|               |",
+       "+---+---+---+---+",
+       "| q | c | d | = |",
+       "+---+---+---+---+",
+       "| 1 | 2 | 3 | + |",
+       "+---+---+---+---+",
+       "| 4 | 5 | 6 | - |",
+       "+---+---+---+---+",
+       "| 7 | 8 | 9 | * |",
+       "+---+---+---+---+",
+       "| 0 | ( | ) | / |",
+       "+---+---+---+---+"]
+
+buttons :: String
+buttons = standard ++ extra
+  where
+    standard = "qcd=123+456-789*0()/"
+    extra    = "QCD \ESC\BS\DEL\n"
+
+getCh                         :: IO Char
+getCh                         =  do hSetEcho stdin False
+                                    c <- getChar
+                                    hSetEcho stdin True
+                                    return c
+
+beep                          :: IO ()
+beep                          =  putStr "\BEL"
+
+cls                           :: IO ()
+cls                           =  putStr "\ESC[2J"
+
+type Pos                      =  (Int,Int)
+ 
+goto                          :: Pos -> IO ()
+goto (x,y)                    =  putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+writeat   :: Pos -> String -> IO ()
+writeat p xs=  do goto p
+                  putStr xs
+
+showbox :: IO ()
+showbox = sequence_ [writeat (1,y) b | (y,b) <- zip [1..] box]
+
+display xs = do writeat (3,2) (replicate 13 ' ')
+                writeat (3,2) (reverse (take 13 (reverse xs)))
+
+calc :: String -> IO()
+calc xs = do 
+  display xs
+  c <- getCh
+  if elem c buttons then
+    process c xs
+  else
+    do beep
+       calc xs
+
+process :: Char -> String -> IO ()
+process c xs | elem c "qQ\ESC" = quit
+             | elem c "dD\BS\DEL" = delete xs
+             | elem c "=\n" = eval' xs
+             | elem c "cC" = clear
+             | otherwise  = press c xs
+
+quit :: IO ()
+quit = goto (1,14)
+
+delete :: String -> IO ()
+delete [] = calc []
+delete xs = calc (init xs)
+
+eval' :: String -> IO ()
+eval' xs = case parse expr xs of
+              [(n,[])] -> calc (show n)
+              _        -> do beep
+                             calc xs
+
+clear :: IO ()
+clear = calc []
+
+press                         :: Char -> String -> IO ()
+press c xs                    =  calc (xs ++ [c])
+
+run :: IO ()
+run = do cls
+         showbox
+         clear
