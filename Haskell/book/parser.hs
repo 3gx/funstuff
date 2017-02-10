3aa43115299c2bea79c15f02f6bdf92d5c917217
Evghenii <eg0x52@gmail.com>
2017-02-10 08:00:39 -0800

    added monadic case

--- Haskell/book/parser.hs
+++ Haskell/book/parser.hs
@@ -8,10 +8,12 @@
 import Control.Applicative
 import Data.Char
 
-newtype Parser a = P (String -> [(a,String)])
+newtype Parser a = P { parse :: String -> [(a,String)] }
 
-parse :: Parser a -> String -> [(a,String)]
-parse (P p) inp = p inp
+-- Alternatively
+-- newtype Parser a = P ( String -> [(a,String)] )
+-- parse :: Parser a -> String -> [(a,String)]
+-- parse (P p) inp = p inp
 
 item :: Parser Char
 item = P (\inp -> case inp of []     -> []
@@ -21,5 +23,56 @@ item = P (\inp -> case inp of []     -> []
 -- []
 -- > parse item "abc"
 -- [('a',"bc")]
---
+
+-- Sequencing parsers
+
+instance Functor Parser where
+  fmap f (P p) = P (\inp -> case p inp of [] -> []
+                                          [(x,out)] -> [(f x, out)])
+
+-- > parse (fmap toUpper item) "abc"
+-- [('A',"bc")]
+-- > parse (fmap toUpper item) ""
+-- []
+
+
+instance Applicative Parser where
+  pure v = P (\inp -> [(v,inp)])
+  (P f) <*> px = 
+       P (\inp -> case f inp of [] -> []
+                                [(g,out)] -> parse (fmap g px) out)
+-- > parse (pure 1) "abc"
+-- [(1,"abc")]
+
+three :: Parser (Char, Char)
+three = pure g <*> item <*> item <*> item
+            where g  x y z = (x,z)
+
+-- > parse three "abcdef"
+-- [(('a','c'),"def")]
+-- > parse three "abc"
+-- [(('a','c'),"")]
+-- > parse three "ab"
+-- []
+
+instance Monad Parser where
+  return v = P(\inp -> [(v,inp)])
+  (P x) >>= f = P (\inp -> case x inp of  
+                                [] -> []
+                                [(v,out)] -> parse (f v) out)
+
+mthree :: Parser (Char, Char)
+mthree = do
+  a <- item
+  item
+  b <- item
+  return (a,b)
+     
+-- > parse mthree "abcdef"
+-- [(('a','c'),"def")]
+-- > parse mthree "abc"
+-- [(('a','c'),"")]
+-- > parse mthree "ab"
+-- []
+
 
