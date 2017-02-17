7655385f5aac5ec4eb7e0fec4070d53cbd2fe2f4
Evghenii <eg0x52@gmail.com>
2017-02-17 08:30:23 -0800

    added some equations

--- Haskell/sharing/observable0.hs
+++ Haskell/sharing/observable0.hs
@@ -1,6 +1,6 @@
 -- Type-Safe Observable sharing
 
-{- shallow embedding 
+-- shallow embedding 
 
 newtype Bit = Bit [Bool] deriving Show
 
@@ -19,29 +19,9 @@ run f bs = rs
 -- > run parity [True, False,True,False]
 -- [True,True,False,False]
 
--}
 
 parity :: Bit -> Bit
 parity input = output
   where 
     output = xor (delay output) input
 
--- deep embedding
-
-data Bit = Xor Bit Bit
-         | Delay Bit
-         | Input [Bool]
-         | Var String
-       deriving Show
-
-xor = Xor
-delay = Delay
-
-run :: (Bit -> Bit) -> [Bool] -> [Bool]
-run f bs = interp (f (Input bs))
-
-interp :: Bit -> [Bool]
-interp (Xor b1 b2) = zipWith (/=) (interp b1) (interp b2)
-interp (Delay b) = False : interp b
-interp (Input bs) = bs
-inputer (Var v) = error $ "Var not supported"
