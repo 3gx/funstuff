23a2143f326df10bede0db84fa72b9efd9a3055c
Evghenii <eg0x52@gmail.com>
2017-02-20 17:18:30 -0800

    +1

--- Haskell/thsk/qq01.hs
+++ Haskell/thsk/qq01.hs
@@ -15,10 +15,10 @@ a = "yeah"
 b = "maybe" 
 person = "jarjar" 
 desc   = "best character" 
-s = unpack [format|$person was $desc, $a, $b, ok maybe not|]
 
 main = do
   print $ makeChunks  "a b c foobar $doobar $goobar. $asdf"
   print $ unpack [format|Hellow World|]
+  let s = unpack [format|$person was $desc, $a, $b, ok maybe not|]
   print s
 
