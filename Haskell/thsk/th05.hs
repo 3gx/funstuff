f8375efd57c319d06af615703fdc92e4c77ef10f
Evghenii <eg0x52@gmail.com>
2017-02-21 08:24:48 -0800

    added link

--- Haskell/thsk/th05.hs
+++ Haskell/thsk/th05.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -ddump-splices #-}
 
+-- https://github.com/leonidas/codeblog/blob/master/2011/2011-12-27-template-haskell.md
+
 import Th05
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
