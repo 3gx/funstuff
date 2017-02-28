6c8ab9500c598ed61ee8737eaaa672692b525afa
Evghenii <eg0x52@gmail.com>
2017-02-28 13:16:23 -0800

    simple qdsl example

--- Haskell/thsk/qdsl01.hs
+++ Haskell/thsk/qdsl01.hs
@@ -1,7 +1,13 @@
 import QDSL01
 
+import Language.Haskell.TH.Lib
+import Language.Haskell.TH
+
 main = do
   let pow1 = $$(power 4)
   print $ pow1 3
   print $ (3*3*3*3 :: Float)
+  expr <- runQ $ power 4
+  putStrLn.pprint.unType $ expr
+  
   return ()
