650e9c9b3a7ab9e0d85b147894fa7e557bb69036
Evghenii <eg0x52@gmail.com>
2017-02-28 12:39:53 -0800

    +1

---
+++ Haskell/thsk/tths02.hs
@@ -0,0 +1,3 @@
+-- type templated haskell
+-- http://ghc-devs.haskell.narkive.com/cCdTaqot/new-branch-implementing-typed-untyped-template-haskell
+-- https://mail.haskell.org/pipermail/ghc-devs/2013-May/001255.html
