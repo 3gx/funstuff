4f9b36460c67cb8a46bca4668504301810fa86c5
Evghenii <eg0x52@gmail.com>
2017-03-07 12:48:23 -0800

    added symbolic

--- Haskell/autodiff/diff1.hs
+++ Haskell/autodiff/diff1.hs
@@ -54,6 +54,53 @@ diffDual (Dual _ x') = x'
 d :: Num a => (Dual a -> Dual c) -> a -> c
 d f x = diffDual . f $ Dual x 1
 
+fib x
+  | x < 0 = 0
+  | x <= 2 = x
+  | otherwise = fib(x - 1) + fib(x - 2)
+
+
+g x y = x ** 2 - 3 * x * y + 2 * y ** 2
+
+infixl 4 :+:
+infixl 5 :*:
+
+-- Type for representing algebraic expressions
+data Expr a
+  = Var Char
+  | Const a
+  | (Expr a) :+: (Expr a)
+  | (Expr a) :*: (Expr a)
+  deriving (Eq, Read, Show)
+
+-- `Num a` implies `Num (Expr a)`
+instance Num a => Num (Expr a) where
+  u + v         = u :+: v
+  u * v         = u :*: v
+  u - v         = u :+: Const (-1) :*: v
+  fromInteger n = Const $ fromInteger n
+  abs u         = undefined
+  signum u      = undefined
+
+-- some stock `Expr a`s to play around with
+-- remember to only use `(+)`, `(-)`, `(*)`, and `fromInteger`
+f1 = 3 * Var 'x' + 4 -- represents `\x -> 3 * x + 4`
+g1 = 4 - Var 'x' * Var 'x' -- represents `\x -> 4 - x * x`
+fx1 = Var 'x' * Var 'x' - 3 * Var 'x'
+
+-- `d` will find the derivative of a haskell function. We need a way to
+-- turn an `Expr a` into a haskell function. `applyExpr` helps us do that.
+
+-- applies `expr` to argument `x`
+applyExpr :: Expr a -> a -> a
+applyExpr expr x = undefined -- exercise left to the reader ;-)
+
+deriv :: (Num a ) => Expr a -> Expr a
+deriv (Var x ) = Const 1
+deriv (Const c) = Const 0
+deriv (u :+: v) = deriv u + deriv v
+deriv (u :*: v) = u  * (deriv v) + (deriv u) * v
+
 main = do
   let f x = x + 1/x
   print $ f  $ Dual 3 1
@@ -72,4 +119,14 @@ main = do
   print $ h 4
   let dh = d h
   print $ dh 4
+  print $ fib 10
+  print $ fib 11
+  print $ d fib 10
+  print $ (fib 10)  + (d fib 10)
+--  let k = g (Dual 2 1) (Dual 1 0) :: Dual Double
+--  print k
+  print $ (f1, deriv f1)
+  print $ (g1, deriv g1)
+  print $ (fx1, deriv fx1)
+
 
