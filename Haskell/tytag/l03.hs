b683f0327b643a05a378602f4406116c441b566d
Evghenii <eg0x52@gmail.com>
2017-03-13 08:59:42 -0700

    working on 3.4

---
+++ Haskell/tytag/l03.hs
@@ -0,0 +1,8 @@
+-- Chapter 3 from lecture.pdf in  
+--   http://okmij.org/ftp/tagless-final/course/index.html
+
+-- 3.4 Tagless final embedding with higher-order abstract syntax
+----------------------------------------------------------------
+
+
+
