059e4872154c53e1441ad692d93fe1c8057f8479
Evghenii <eg0x52@gmail.com>
2017-03-14 09:55:42 -0700

    added locks

--- Haskell/fun-with-types/phantom.hs
+++ Haskell/fun-with-types/phantom.hs
@@ -59,7 +59,92 @@ g = fetch32 p2 -- compiles because p2 ha Pointer Four
 -- 5.2 Tracking state & control in a parametrised monad
 -------------------------------------------------------
 
+class PMonad m where
+  unit :: a -> m p p a
+  bind :: m p q a -> (a -> m q r b) -> m p r b
+
+-- start phantom
+data Nil
+data Cons l s
+
+data Locked
+data Unlocked
+-- end phantom
+
+newtype LockM p q a = LockM { unLockM :: IO a }
+
+instance PMonad LockM where
+  unit x   = LockM (return x)
+  bind m k = LockM (unLockM m >>= unLockM . k)
+
+lput :: String -> LockM p p ()
+lput = LockM . putStrLn
+
+type family Get n p
+type instance Get Zero (Cons e p) = e
+type instance Get (Succ n) (Cons e p) = Get n p
+
+type family Set n e' p
+type instance Set Zero e' (Cons e p) = Cons e' p
+type instance Set (Succ n) e' (Cons e p) = Cons e (Set n e' p)
+
+newtype Lock n = Lock Int deriving Show
+
+mkLock :: forall n . Nat n => Lock n
+mkLock = Lock (toInt (undefined :: n))
+
+lock1 = mkLock :: Lock One
+lock4 = mkLock :: Lock Four
+
+acquire :: (Get n p ~ Unlocked) => Lock n -> LockM p (Set n Locked p) ()
+acquire l = LockM (putStrLn ("acquire" ++ show l))
+
+release :: (Get n p ~ Locked) => Lock n -> LockM p (Set n Unlocked p) ()
+release l = LockM (putStrLn ("release" ++ show l))
+
+type ThreeLocks = Cons Unlocked (Cons Unlocked (Cons Unlocked Nil))
+run :: LockM ThreeLocks ThreeLocks a -> IO a
+run = unLockM 
+
+with1 a = acquire lock1 `bind` \_ ->
+          a `bind` \x ->
+          release lock1 `bind` \_ ->
+          unit x
+
+with02 a = acquire_locks `bind` \_ ->
+          a `bind` \x ->
+          release_locks `bind` \_ ->
+          unit x
+ where
+ -- We acquire and release in the same order. Not a LIFO
+ acquire_locks = acquire (mkLock::Lock Zero) `bind` \_ -> 
+     acquire (mkLock::Lock Two)
+ release_locks = release (mkLock::Lock Zero) `bind` \_ ->
+     release (mkLock::Lock Two)
+
+with01 a = acquire_locks `bind` \_ ->
+          a `bind` \x ->
+          release_locks `bind` \_ ->
+          unit x
+ where
+ -- We acquire and release in the same order. Not a LIFO
+ acquire_locks = acquire (mkLock::Lock One) `bind` \_ -> 
+     acquire (mkLock::Lock Two)
+ release_locks = release (mkLock::Lock Two) `bind` \_ ->
+     release (mkLock::Lock One)
+
+critical1 :: (Get One p ~ Locked) => LockM p p ()
+critical1 = LockM (putStrLn "Critical section 1")
+
 main = do
   print $ toInt (undefined :: Two)
   print $ toInt (undefined :: Eight)  
+  print "--------"
+  run (with1 (lput "hello"))
+  print "--------"
+  run (with02 (lput "hello again"))
+  print "--------"
+  run (with1 critical1)
+  print "--------"
+  run (with01 critical1)
 
