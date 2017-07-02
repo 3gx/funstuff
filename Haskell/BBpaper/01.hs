5782a5220af8f9143cdcf1d0e4c55c87a947a2c7
egaburov <eg0x52@gmail.com>
2017-07-02 14:04:33 -0700

    +wip

--- Haskell/BBpaper/01.hs
+++ Haskell/BBpaper/01.hs
@@ -3,6 +3,7 @@
 import Control.Monad
 
 {- API:
+   ----
    move      :: Program ()
    turnLeft  :: Program ()
    turnRight :: Program ()
@@ -43,12 +44,57 @@ instance Monad Program where
 
 
 instance Applicative Program  where
-  pure = Return
-  (<*>)  = undefined
+  pure = return
+  mf <*> mx = do
+    f <- mf
+    x <- mx
+    return $ f x
 
 instance Functor Program  where
-  fmap = undefined
+  fmap f mx = do
+    x <- mx
+    return $ f x
 
+move = undefined
+turnLeft = undefined
 
+spiralIn :: Int -> Program ()
+spiralIn 0 = return ()
+spiralIn n = do
+  replicateM_ 2 $ do
+    replicateM_ n move
+    turnLeft
+  spiralIn (n-1)
             
 
+data Prg = PMove
+         | PTurnRight
+         | PTurnLeft
+         | PSensor Name
+         | PCond BoolE Prg Prg
+         | PWhile Name Prg Prg
+
+         -- PSeq replaced bind
+         | PSeq Prg Prg
+         | PSkip
+         
+         -- PAssign is used by While in Program
+         | PAssign Name BoolE
+
+newNameSupply = undefined
+supplyValue x = 42
+
+runCompile :: Program a -> Prg
+runCompile prg = snd $ compile s prg
+  where s = newNameSupply
+
+-- compile :: NAmeSupply -> Program a -> (a, Prg)
+compile :: a -> Program a -> (a, Prg)
+compile s Move = ((), PMove)
+compile s TurnRight = ((), PTurnRight)
+compile s TurnLeft = ((), PTurnLeft)
+
+compile s Sensor = (Var nom, PSensor nom)
+  where
+      v = supplyValue s
+      nom = "v" ++ show v
